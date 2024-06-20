68a42d50360e4b496e59ddcba83de58b36d38642
saurabh kumar <saurabhkumar9493@gmail.com>
2024-06-20 17:52:53 +0530

    array

--- class6_array.js
+++ class6_array.js
@@ -1,4 +1,4 @@
-let arr = [1,2,3,"suraj"];
+// let arr = [1,2,3,"suraj"];
 
 // console.log(typeof(arr))
 
@@ -24,4 +24,107 @@ let arr = [1,2,3,"suraj"];
     // }
 
 
+    // 2. push()
+    // let arr = [1,5,6,4];
+
+    //       arr.push(12);
+    //       arr.push(100);
+    //       arr.pop()
+    //       arr.pop()
+    //       arr.pop()
+    //       console.log(arr)
+
+
+    // 3. unshift()
+
+    // let arr = ["kullu","manali",true,12];
+
+    //       arr.unshift("shimla")
+    //       arr.unshift("delhi")
+    //       arr.shift()
+    //       console.log(arr)
+
+
+//    4. Array.isArray()
+
+            // let arr = [12,4,5,6]
+            // // let arr = "shimla"
+
+            // console.log(Array.isArray(arr))
+
+
+    // 5. includes
+
+    // let arr = [1,5,8,6,123456]
+    // // console.log(arr.includes(6))
+    //  let pass = 123456
+    // if(arr.includes(pass)){
+    //     console.log("login successfully")
+    // }else{
+    //     console.log("your password is incorrect")
+    // }
+
+
+    // 6.indexOf()
+
+    // let arr = [1,2,8,6];
+    // console.log(arr.indexOf(10))
+
+
+    // 7. slice()
+
+    // let arr = [4,5,6,99,45,66]
+    // // console.log(arr.slice())
+    // // console.log(arr.slice(2,4))
+    // console.log(arr.slice(-5,-2))
+
+    // 8.join()
+
+    // let arr = ["kullu","manali","shimla","spiti"]
+    // console.log(arr.join(" "))
+
+
+    // 9. splice()
+
+    //    let arr = [10,20,30,40,50,60]
+              
+    //          arr.splice(2,3,456)
+    //    console.log(arr)
+
+    // 10. fill()
+    //    let arr = [10,20,30,40,50,60]
+           
+    //         arr.fill(22,2,5)
+    //         console.log(arr)
+
+
+    // 11. reverse()
+
+    // let arr = [11,10,22,40,60]
+    //      arr.reverse()
+    //      console.log(arr)
+
+
+    // 12. sort
+
+    // let arr = [1,45,12,8,66]
+        //   let arr = ["komal","shimla","kullu","aman"]
+        //   arr.sort()
+        //   console.log(arr)
+
+
     
+        // let arr = [[1,2,3],["shimla","kullu"],[77,55,66]]
+
+        // console.log(arr[2][1])
+
+
+        //   console.log(1)
+        //   console.log(2)
+        //   console.log(3)
+        //   console.log(4)
+        //   console.log(5)
+
+
+        // ab start hota h loop
+
